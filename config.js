9bb178d83dc1481a8280cfcb6140f91efabd761e
Functionfreak196 <kanyoroelias936@gmail.com>
2025-02-18 08:06:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2VVSSToI#XbauT9Vb0dl9827ui3fFqUYELdMiLvVA0kdQza6b5yI",  // DO NOT CHANGE THIS !!!
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/FLSgNhW9/Free.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that HansByte Md is ALIVE now—ready to dive into new adventures with you!",
-SUDO_NB: process.env.SUDO_NB || "237696900612",
+SUDO_NB: process.env.SUDO_NB || "254786182066",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true",
 OMDB_API_KEY: "5e339fb7",
-OWNER_NAME: process.env.OWNER_NAME || "Harold Mth",
+OWNER_NAME: process.env.OWNER_NAME || "elias",
 AUTO_TYPING: process.env.AUTO_TYPING || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true"
 };
